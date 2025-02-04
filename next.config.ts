35e6e5b75ec786ca057323073e3c0e7f316eba88
Vipul Suthar <vipulsuthar1144@gmail.com>
2025-02-04 13:21:28 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["*"], // Add external domains here
+    // domains: ["*"], // Add external domains here
     remotePatterns: [
       {
         protocol: "https",
